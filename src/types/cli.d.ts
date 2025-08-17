2d1c2cb2041d87d05c5650e05c22b3a2f53611c2
mandrillx <mandrillx@protonmail.com>
2025-08-17 00:25:13 -0400

    Update project dependencies and configuration by removing pnpm-lock.yaml, adjusting typeRoots in tsconfig.json, and modifying package.json scripts. The README.md file has also been deleted as part of the cleanup process.

--- src/types/cli.d.ts
+++
@@ -1,4 +0,0 @@
-// TODO: Fix the type issues properly, it's due to the typeRoots containing @bloxstack for @bloxstack/sdk.
-// This is a temporary fix to allow the project to build.
-
-declare module "cli" {}
